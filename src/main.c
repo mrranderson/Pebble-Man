cc668948f61f6ee3002b3ff398bb08b99824a2e6
mrranderson <ry.sc.anderson@gmail.com>
2014-04-11 16:41:33 -0700

    Select Button does things

--- src/main.c
+++ src/main.c
@@ -1,21 +1,49 @@
 #include <pebble.h>
 
-Window *my_window;
+Window *window;
 TextLayer *text_layer;
 
-void handle_init(void) {
-  my_window = window_create();
 
-  text_layer = text_layer_create(GRect(0, 0, 144, 20));
+void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Select");
+}
+
+void config_provider(void *context) {
+   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+ }
+
+void handle_init(void) {
+	// Create a window and text layer
+	window = window_create();
+	text_layer = text_layer_create(GRect(0, 0, 144, 154));
+	
+	// Set the text, font, and text alignment
+	text_layer_set_text(text_layer, "Welcome to \nPebbleMan");
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	
+	// Add the text layer to the window
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  
+ window_set_click_config_provider(window, config_provider);
+
+	// Push the window
+	window_stack_push(window, true);
+	
+	// App Logging!
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
 }
 
 void handle_deinit(void) {
-  text_layer_destroy(text_layer);
-  window_destroy(my_window);
+	// Destroy the text layer
+	text_layer_destroy(text_layer);
+	
+	// Destroy the window
+	window_destroy(window);
 }
 
 int main(void) {
-  handle_init();
-  app_event_loop();
-  handle_deinit();
+	handle_init();
+	app_event_loop();
+	handle_deinit();
 }
